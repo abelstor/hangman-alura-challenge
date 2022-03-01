dd3ee86d75127abd65b39936da0859b0aa615c4a
abelstor <abelstor@gmail.com>
2022-02-28 21:40:36 -0500

    swal added

--- ahorcado.js
+++ ahorcado.js
@@ -1,6 +1,7 @@
 
 const iniciarJuego = document.querySelector('#iniciar-juego');
 const nuevaPalabra = document.querySelector('#nueva-palabra');
+const nuevoJuego = document.querySelector('#nuevo-juego');
 
 let palabras = ['alura', 'oracle', 'html', 'css', 'javascript', 'hangman', 'abel'];
 
@@ -9,13 +10,89 @@ nuevaPalabra.addEventListener('click', (e) => {
     e.preventDefault();
     const text = document.querySelector('#input-nueva-palabra');
     palabras = [...palabras, (text.value)];
+    console.log(palabras);
     document.getElementById('input-nueva-palabra').value = '';
 })
 
 iniciarJuego.addEventListener('click', () => {
 
+    let intentos = 0;
+    let gano = false;
+    let game = [];
+
     const palabraSecreta = palabras[Math.floor(Math.random() * palabras.length)];
     espaciosPalabra(palabraSecreta.length);
-    console.log(palabraSecreta);
+
+    const keyGame = (key) => {
+        const secretKey = palabraSecreta.includes(key);
+
+        if (!secretKey) {
+            intentos++;
+        } else {
+            dibujarLetras(palabraSecreta, key);
+            secret = [...palabraSecreta];
+            game = [...game, key];
+
+            let secretWord = secret.filter((item, idx) => {
+                return secret.indexOf(item) === idx;
+            })
+
+            secretWord.sort();
+            game.sort();
+
+            if (secretWord.every((ele, ix) => { return ele === game[ix] })) {
+                gano = true;
+            }
+        }
+
+        if (gano) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Ganaste!  :)',
+                showConfirmButton: true,
+            })
+        }
+
+        switch (intentos) {
+            case 1:
+                dibujarLargo();
+                break;
+            case 2:
+                dibujarHorizontal();
+                break;
+            case 3:
+                dibujarCorto();
+                break;
+            case 4:
+                dibujarCabeza();
+                break;
+            case 5:
+                dibujarCuerpo();
+                break;
+            case 6:
+                dibujarPiernaIzquierda();
+                break;
+            case 7:
+                dibujarPiernaDerecha();
+                break;
+            case 8:
+                dibujarBrazoIzquierdo();
+                break;
+            case 9:
+                dibujarBrazoDerecho();
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Perdiste!  :(',
+                    showConfirmButton: true,
+                })
+                break;
+            default:
+                break;
+        }
+    }
+
+    window.addEventListener('keypress', ({ key }) => { keyGame(key) }, false);
 
 })
+// window.removeEventListener('click', () => { });
+window.removeEventListener('keypress', () => { keyGame }, true);
--- canvas.js
+++ canvas.js
@@ -2,50 +2,134 @@
 let pantalla = document.getElementById('ahorcado');
 let pincel = pantalla.getContext('2d');
 
-pincel.fillStyle = 'darkgrey';
-pincel.fillRect(0, 0, 1200, 800);
-pincel.fillStyle = 'black';
-pincel.lineWidth = 5;
+const dibujarTablero = () => {
+    pincel.fillStyle = 'darkgrey';
+    pincel.fillRect(0, 0, 1200, 800);
+    pincel.fillStyle = 'black';
+    pincel.lineWidth = 5;
+    pincel.fillRect(198, 572, 70, 20); // base
+}
+dibujarTablero();
 
-pincel.fillRect(198, 572, 70, 20); // base
-pincel.fillRect(230, 100, 5, 472); // vertical largo
-pincel.fillRect(230, 100, 200, 5); // horizontal
-pincel.fillRect(430, 100, 5, 30); // vertical pequeño
+const dibujarLargo = () => {
+    pincel.fillRect(230, 100, 5, 472);
+}
 
-pincel.beginPath();
-pincel.arc(432, 170, 40, 0, 2 * 3.14); // cabeza
-pincel.stroke();
+const dibujarHorizontal = () => {
+    pincel.fillRect(230, 100, 200, 5);
+}
 
-pincel.fillRect(430, 210, 5, 190); // tronco
+const dibujarCorto = () => {
+    pincel.fillRect(430, 100, 5, 30);
+}
 
-pincel.beginPath(); // Brazo derecho
-pincel.moveTo(432, 270);
-pincel.lineTo(530, 200);
-pincel.closePath();
-pincel.stroke();
+const dibujarCabeza = () => {
+    pincel.beginPath();
+    pincel.arc(432, 170, 40, 0, 2 * 3.14);
+    pincel.strokeStyle = '#000';
+    pincel.stroke();
+}
 
-pincel.beginPath(); // Brazo izquierdo
-pincel.moveTo(432, 270);
-pincel.lineTo(330, 200);
-pincel.closePath();
-pincel.stroke();
+const dibujarCuerpo = () => {
+    pincel.fillRect(430, 210, 5, 190);
+}
 
-pincel.beginPath(); // Pierna derecha
-pincel.moveTo(432, 400);
-pincel.lineTo(520, 470);
-pincel.closePath();
-pincel.stroke();
+const dibujarBrazoDerecho = () => {
+    pincel.beginPath();
+    pincel.moveTo(432, 270);
+    pincel.lineTo(530, 200);
+    pincel.closePath();
+    pincel.strokeStyle = '#000';
+    pincel.stroke();
+}
 
-pincel.beginPath(); // Pierna izquierda
-pincel.moveTo(432, 400);
-pincel.lineTo(340, 470);
-pincel.closePath();
-pincel.stroke();
+const dibujarBrazoIzquierdo = () => {
+    pincel.beginPath();
+    pincel.moveTo(432, 270);
+    pincel.lineTo(330, 200);
+    pincel.closePath();
+    pincel.strokeStyle = '#000';
+    pincel.stroke();
+}
+const dibujarPiernaDerecha = () => {
+    pincel.beginPath();
+    pincel.moveTo(432, 400);
+    pincel.lineTo(520, 470);
+    pincel.closePath();
+    pincel.strokeStyle = '#000';
+    pincel.stroke();
+}
+const dibujarPiernaIzquierda = () => {
+    pincel.beginPath();
+    pincel.moveTo(432, 400);
+    pincel.lineTo(340, 470);
+    pincel.closePath();
+    pincel.strokeStyle = '#000';
+    pincel.stroke();
+}
 
 const espaciosPalabra = (num) => {
-
+    // pincel.fillStyle = 'green';
     for (let i = 1; i <= num; i++) {
         let x = 300 + (i * 70);
         pincel.fillRect(x, 580, 50, 5);
     }
+}
+
+const dibujarLetras = (palabraSecreta, key) => {
+
+    pincel.font = 'normal 40px Georgia';
+    pincel.strokeStyle = '#0a3871';
+
+    let letras = [];
+
+    for (const letra of palabraSecreta) {
+        letras = [...letras, letra];
+    }
+
+    letras.forEach((letra, index) => {
+
+        if (letra === key) {
+
+            switch (index) {
+                case 0:
+                    pincel.strokeText(key, 385, 560);
+                    break;
+                case 1:
+                    pincel.strokeText(key, 455, 560);
+                    break;
+                case 2:
+                    pincel.strokeText(key, 525, 560);
+                    break;
+                case 3:
+                    pincel.strokeText(key, 595, 560);
+                    break;
+                case 4:
+                    pincel.strokeText(key, 665, 560);
+                    break;
+                case 5:
+                    pincel.strokeText(key, 735, 560);
+                    break;
+                case 6:
+                    pincel.strokeText(key, 805, 560);
+                    break;
+                case 7:
+                    pincel.strokeText(key, 875, 560);
+                    break;
+                case 8:
+                    pincel.strokeText(key, 945, 560);
+                    break;
+                case 9:
+                    pincel.strokeText(key, 1015, 560);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+}
+
+const clearCanvas = () => {
+    pincel.clearRect(0, 0, pantalla.width, pantalla.height);
+    dibujarTablero();
 }
